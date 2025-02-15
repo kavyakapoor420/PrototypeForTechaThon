ae37f0c01a620532083e5721a46d68ccfb8a9c20
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-02-15 12:39:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Chatbot from "./Components/Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-400">
+        Hello world 
+      </h1>
+    <Chatbot/>
+    </div>
+    
+  );
+}
+
+export default App;
